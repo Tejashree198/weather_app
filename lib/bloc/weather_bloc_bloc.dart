e59ca0a507b9f680de04e9886d9f8eac6737a99b
Tejashree Jagtap <tejashreejagtap198@gmail.com>
2024-04-11 16:31:52 +0530

    initial commit

---
+++ lib/bloc/weather_bloc_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
+
+part 'weather_bloc_event.dart';
+part 'weather_bloc_state.dart';
+
+class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
+  WeatherBlocBloc() : super(WeatherBlocInitial()) {
+    on<WeatherBlocEvent>((event, emit) async {
+      emit(WeatherBlocLoading());
+      try {
+        WeatherFactory wf =
+            WeatherFactory("API_KEY", language: Language.ENGLISH);
+
+        Position position = await Geolocator.getCurrentPosition();
+
+        Weather weather = await wf.currentWeatherByLocation(
+          event.position.latitude,
+          event.position.longitude,
+        );
+        print(weather);
+        emit(WeatherBlocSuccess(weather));
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
+    });
+  }
+}
