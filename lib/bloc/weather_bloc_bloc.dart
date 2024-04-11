c5fd70a26fbce5d326aca644c1eb3a3ce2aea255
Tanmay Bharambe <bharambetr2002@gmail.com>
2024-04-11 22:31:18 +0530

    Final Source Code

--- lib/bloc/weather_bloc_bloc.dart
+++ lib/bloc/weather_bloc_bloc.dart
@@ -1,19 +1,19 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/weather.dart';
-
+import 'package:weather_app/data/my_data.dart';
 part 'weather_bloc_event.dart';
 part 'weather_bloc_state.dart';
 
+
 class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
   WeatherBlocBloc() : super(WeatherBlocInitial()) {
-    on<WeatherBlocEvent>((event, emit) async {
+    on<FetchWeather>((event, emit) async {
       emit(WeatherBlocLoading());
       try {
         WeatherFactory wf =
-            WeatherFactory("API_KEY", language: Language.ENGLISH);
+            WeatherFactory(API_KEY, language: Language.ENGLISH);
 
         Position position = await Geolocator.getCurrentPosition();
 
@@ -21,7 +21,6 @@ class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
           event.position.latitude,
           event.position.longitude,
         );
-        print(weather);
         emit(WeatherBlocSuccess(weather));
       } catch (e) {
         emit(WeatherBlocFailure());
